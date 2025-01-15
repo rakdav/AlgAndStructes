92f4d4a68be26f1a743b03b40c24012cd61af003
Andrews <rakdav@inbox.ru>
2025-01-15 18:14:56 +0200

    hhhh

--- Deykstra/Program.cs
+++ Deykstra/Program.cs
@@ -1,4 +1,30 @@
-﻿//вершина графа
+﻿var g = new Graph();
+g.AddVertex("A");
+g.AddVertex("B");
+g.AddVertex("C");
+g.AddVertex("D");
+g.AddVertex("E");
+g.AddVertex("F");
+g.AddVertex("G");
+
+g.AddEdge("A", "B", 22);
+g.AddEdge("A", "C", 33);
+g.AddEdge("A", "D", 61);
+g.AddEdge("B", "C", 47);
+g.AddEdge("B", "E", 93);
+g.AddEdge("C", "D", 11);
+g.AddEdge("C", "E", 79);
+g.AddEdge("C", "F", 63);
+g.AddEdge("D", "F", 41);
+g.AddEdge("E", "F", 17);
+g.AddEdge("E", "G", 58);
+g.AddEdge("F", "G", 84);
+
+var geikstra = new Dijkstra(g);
+var path = geikstra.FindShortestPath("A", "E");
+Console.WriteLine(path);
+
+//вершина графа
 public class GraphVertex
 {
     public string? Name { get; }
