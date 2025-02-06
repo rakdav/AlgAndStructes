a19cbf4c9524e02620cd815861f5ff482c9b3dcb
rakdav <rakdav@inbox.ru>
2025-02-06 13:33:44 +0200

    b2

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,46 @@
+﻿public class TreeNode<T>
+{
+    public T Data { get; set; }
+    public TreeNode<T> Parent { get; set; }
+    public List<TreeNode<T>> Children { get; set; }
+    public int GetHeight()
+    {
+        int height = 1;
+        TreeNode<T> current = this;
+        while (current.Parent != null)
+        {
+            height++;
+            current = current.Parent;
+        }
+        return height;
+    }
+}
+class BinaryTreeNode<T> : TreeNode<T>
+{
+    public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null!, null! };
+    public BinaryTreeNode<T> Left
+    {
+        get { return (BinaryTreeNode<T>)Children[0]; }
+        set { Children[0] = value; }
+    }
+    public BinaryTreeNode<T> Right
+    {
+        get { return (BinaryTreeNode<T>)Children[1]; }
+        set { Children[1] = value; }
+    }
+}
+class BinaryTree<T>
+{
+    public BinaryTreeNode<T> Root { get; set; }
+    public int Count { get; set; }
+    private void TraversePreOrder(BinaryTreeNode<T> node,
+        List<BinaryTreeNode<T>> result)
+    {
+        if (node != null)
+        {
+            result.Add(node);
+            TraversePreOrder(node.Left, result);
+            TraversePreOrder(node.Right, result);
+        }
+    }
+}
