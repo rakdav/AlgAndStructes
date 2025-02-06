28c13bf9a929a05019366f646948e087700d065a
rakdav <rakdav@inbox.ru>
2025-02-06 12:58:18 +0200

    Person

--- BasicTree/Program.cs
+++ BasicTree/Program.cs
@@ -10,6 +10,34 @@ tree.Root.Children[2].Children = new List<TreeNode<int>>
 {
     new TreeNode<int>{Data=30,Parent=tree.Root.Children[2]}
 };
+Tree<Person> company = new Tree<Person>();
+company.Root = new TreeNode<Person>()
+{
+    Data = new Person(100, "Маша с кухни", "Кулинар"),
+    Parent=null!
+};
+company.Root.Children = new List<TreeNode<Person>>()
+{
+    new TreeNode<Person>()
+    {
+        Data=new Person(1,"Арина","SuperWoman"),
+        Parent=company.Root
+    },
+    new TreeNode<Person>()
+    {
+        Data=new Person(10,"Сорока","Коллекционер пугатель"),
+        Parent=company.Root
+    },
+     new TreeNode<Person>()
+    {
+        Data=new Person(20,"Хомячков","Сосед сороки"),
+        Parent=company.Root
+    }
+};
+company.Root.Children[2].Children = new List<TreeNode<Person>>()
+{
+
+};
 class TreeNode<T>
 {
     public T Data { get; set; }
@@ -30,4 +58,19 @@ class TreeNode<T>
 class Tree<T>
 {
     public TreeNode<T> Root { get; set; }
+}
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public Person()
+    {
+    }
+    public Person(int id, string name, string role)
+    {
+        Id = id;
+        Name = name;
+        Role = role;
+    }
 }
