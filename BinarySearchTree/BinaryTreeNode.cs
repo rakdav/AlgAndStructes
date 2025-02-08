70bd231b590bf646b879b49cea118dd7cd9899b3
rakdav <rakdav@inbox.ru>
2025-02-08 13:21:07 +0200

    bst

---
+++ BinarySearchTree/BinaryTreeNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTreeNode<T>:TreeNode<T>
+    {
+        public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null!, null! };
+        public BinaryTreeNode<T> Parent { get; set; }
+    }
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>>? Children { get; set; }
+    }
+}
--- BinaryTree/TreeNode.cs
+++ BinaryTree/TreeNode.cs
@@ -9,8 +9,8 @@ namespace BinaryTree
     public class TreeNode<T>
     {
         public T Data { get; set; }
-        public TreeNode<T> Parent { get; set; }
-        public List<TreeNode<T>> Children { get; set; }
+        public TreeNode<T>? Parent { get; set; }
+        public List<TreeNode<T>>? Children { get; set; }
         public int GetHeight()
         {
             int height = 1;
