4a359838117fc05ba301a451490308b9201d4459
rakdav <rakdav@inbox.ru>
2025-02-20 10:54:54 +0200

    BST

--- BinarySearchTree/BinaryTreeNode.cs
+++ BinarySearchTree/BinaryTreeNode.cs
@@ -8,176 +8,24 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BinarySearchTree
 {
-    public class BinarySearchTree<T>:BinaryTree<T> where T : IComparable 
+    public class BinaryTreeNode<T> : TreeNode<T>
     {
-        
-        public bool Contains(T data)
-        {
-            BinaryTreeNode<T> node = Root;
-            while (node != null)
-            {
-                int result = data.CompareTo(node.Data);
-                if (result == 0) return true;
-                else if (result < 0) node = node.Left;
-                else node = node.Right;
-            }
-            return false;
-        }
-        public void Add(T data)
-        {
-            BinaryTreeNode<T> parent = GetParentForNewNode(data);
-            BinaryTreeNode<T> node = new BinaryTreeNode<T>()
-            {
-                Data = data,
-                Parent = parent
-            };
-            if (parent == null) Root = node;
-            else if (data.CompareTo(parent.Data) < 0) parent.Left = node;
-            else parent.Right = node;
-            Count++;
-        }
-        private BinaryTreeNode<T> GetParentForNewNode(T data)
-        {
-            BinaryTreeNode<T> current = Root;
-            BinaryTreeNode<T> parent = null;
-            while (current!=null)
-            {
-                parent = current;
-                int result = data.CompareTo(current.Data);
-                if (result == 0) throw new ArgumentException($"Узел {data} уже существует.");
-                else if (result < 0) current = current.Left;
-                else current = current.Right;
-            }
-            return parent!;
-        }
-        public void Remove(T data)
-        {
-            Remove(Root, data);
-        }
-        public void Remove(BinaryTreeNode<T> node,T data)
-        {
-            if (node == null) throw new ArithmeticException($"Узел {data} не существует");
-            else if (data.CompareTo(node.Data) < 0) Remove(node.Left, data);
-            else if (data.CompareTo(node.Data) > 0) Remove(node.Right, data);
-            else
-            {
-                if (node.Left == null && node.Right == null)
-                {
-                    ReplaceInParent(node, null!);
-                    Count--;
-                }
-                else if (node.Right == null)
-                {
-                    ReplaceInParent(node, node.Left!);
-                    Count--;
-                }
-                else if (node.Left == null)
-                {
-                    ReplaceInParent(node, node.Right);
-                    Count--;
-                }
-                else
-                {
-                    BinaryTreeNode<T> successor = FindMinimumInSubtree(node.Right);
-                    node.Data = successor.Data;
-                    Remove(successor, successor.Data);
-                }
-            }
-        }
-        private void ReplaceInParent(BinaryTreeNode<T>node, BinaryTreeNode<T> newNode)
-        {
-            if (node.Parent != null)
-            {
-                if (node.Parent.Left == node) node.Parent.Left = newNode;
-                else node.Parent.Right = newNode;
-            }
-            else Root = newNode;
-            if (newNode != null) newNode.Parent = node.Parent!;
-        }
-        private BinaryTreeNode<T> FindMinimumInSubtree(BinaryTreeNode<T> node)
-        {
-            while (node.Left != null) node = node.Left;
-            return node;
-        }
-    }
-    public class BinaryTree<T>
-    {
-        public BinaryTreeNode<T> Root { get; set; }
-        public int Count { get; set; }
-        public List<BinaryTreeNode<T>> Traverse(TraversalEnum mode)
-        {
-            List<BinaryTreeNode<T>> nodes = new List<BinaryTreeNode<T>>();
-            switch (mode)
-            {
-                case TraversalEnum.PREORDER:
-                    TraversalPreOrder(Root, nodes);
-                    break;
-                case TraversalEnum.INORDER:
-                    TraversalInOrder(Root, nodes);
-                    break;
-                case TraversalEnum.POSTORDER:
-                    TraversalPostOrder(Root, nodes);
-                    break;
-            }
-            return nodes;
-        }
-        private void TraversalPreOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                result.Add(node);
-                TraversalPreOrder(node.Left, result);
-                TraversalPreOrder(node.Right, result);
-            }
-        }
-        private void TraversalInOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                TraversalInOrder(node.Left, result);
-                result.Add(node);
-                TraversalInOrder(node.Right, result);
-            }
-        }
-        private void TraversalPostOrder(BinaryTreeNode<T> node, List<BinaryTreeNode<T>> result)
-        {
-            if (node != null)
-            {
-                TraversalPostOrder(node.Left, result);
-                TraversalPostOrder(node.Right, result);
-                result.Add(node);
-            }
-        }
-        public int GetHeight()
-        {
-            int height = 0;
-            foreach (BinaryTreeNode<T> node in Traverse(TraversalEnum.PREORDER))
-            {
-                height = Math.Max(height, node.GetHeight());
-            }
-            return height;
-        }
-    }
-    public enum TraversalEnum
-    {
-        PREORDER,
-        INORDER,
-        POSTORDER
-    }
-    public class BinaryTreeNode<T>:TreeNode<T>
-    {
-        public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null!, null! };
+        public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null, null };
+
         public BinaryTreeNode<T> Parent { get; set; }
+
         public BinaryTreeNode<T> Left
         {
             get { return (BinaryTreeNode<T>)Children[0]; }
             set { Children[0] = value; }
         }
+
         public BinaryTreeNode<T> Right
         {
             get { return (BinaryTreeNode<T>)Children[1]; }
             set { Children[1] = value; }
         }
+
         public int GetHeight()
         {
             int height = 1;
@@ -189,10 +37,5 @@ namespace BinarySearchTree
             }
             return height;
         }
-    }
-    public class TreeNode<T>
-    {
-        public T Data { get; set; }
-        public List<TreeNode<T>>? Children { get; set; }
     }
 }
