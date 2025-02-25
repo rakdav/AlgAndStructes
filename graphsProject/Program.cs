4dcaa0602dd7b0ac0f6edc8b4b67301989d013b3
rakdav <rakdav@inbox.ru>
2025-02-25 14:57:57 +0200

    nwno

--- graphsProject/Program.cs
+++ graphsProject/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using graphsProject;
+//неориентированный, без веса ребер
+Graph<int> graph = new Graph<int>(false, false);
+Node<int> n1 = graph.AddNode(1);
+Node<int> n2 = graph.AddNode(2);
+Node<int> n3 = graph.AddNode(3);
+Node<int> n4 = graph.AddNode(4);
+Node<int> n5 = graph.AddNode(5);
+Node<int> n6 = graph.AddNode(6);
+Node<int> n7 = graph.AddNode(7);
+Node<int> n8 = graph.AddNode(8);
+
+graph.AddEdge(n1, n2);
+graph.AddEdge(n1, n3);
+graph.AddEdge(n2, n4);
+graph.AddEdge(n3, n4);
+graph.AddEdge(n4, n5);
+graph.AddEdge(n5, n6);
+graph.AddEdge(n5, n8);
+graph.AddEdge(n5, n7);
+graph.AddEdge(n7, n8);
+graph.AddEdge(n6, n7);
