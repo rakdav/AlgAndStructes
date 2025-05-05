40cf66abbdef0dfb2cfc455ee5055c8e94c8b22c
rakdav <rakdav@inbox.ru>
2025-05-05 18:08:03 +0200

    last5.05

--- FirstMongo/Person.cs
+++ FirstMongo/Person.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,13 @@ namespace FirstMongo
     class Person
     {
         public ObjectId Id { get; set; }
+        //[BsonId]
+        //public int PersonId { get; set; }
         public string? Name { get; set; }
+        [BsonElement("Years")]
+        [BsonIgnoreIfDefault]
         public int Age { get; set; }
+        [BsonIgnoreIfNull]
         public Company? Company { get; set; }
         public List<string>? Languages { get; set; } = new List<string>();
     }
--- FirstMongo/Program.cs
+++ FirstMongo/Program.cs
@@ -16,6 +16,8 @@ MongoClient client = new MongoClient("mongodb://localhost:27017");
 
 //Создание коллекции
 var db = client.GetDatabase("test");
+var users = db.GetCollection<BsonDocument>("users");
+var collection= db.GetCollection<Person>("users");
 //await db.CreateCollectionAsync("people");
 
 //Получение списка коллекций
@@ -67,12 +69,109 @@ var db = client.GetDatabase("test");
 //Person person = new Person { Name = "Tom", Age = 38 };
 //person.Company = new Company { Name = "Microsoft" };
 //Console.WriteLine(person.ToJson());
-BsonDocument doc = new BsonDocument
+
+//из BsonDocument в person
+//BsonDocument doc = new BsonDocument
+//{
+//    {"name","Tom"},
+//    {"age", 38},
+//    { "company", new BsonDocument{{"name" , "microsoft"}}},
+//    {"languages", new BsonArray{"english", "german", "spanish" } }
+//};
+//Person person = BsonSerializer.Deserialize<Person>(doc);
+//Console.WriteLine(person.ToJson());
+
+
+//Сохранение документов в базу данных
+//Person->BsonDocument
+//Person person = new Person
+//{
+//    Name = "Tom",
+//    Age = 38,
+//    Company = new Company { Name = "Microsoft" },
+//    Languages = { "english", "german", "spannish" }
+//};
+//BsonDocument doc = person.ToBsonDocument();
+//Console.WriteLine(doc);
+//var users = db.GetCollection<BsonDocument>("users");
+//await users.InsertOneAsync(doc);
+//Person person2 = new Person
+//{
+//    Name = "Bob",
+//    Age = 25,
+//    Company = new Company { Name = "Samsung" },
+//    Languages = { "english","spannish" }
+//};
+//Person person3 = new Person
+//{
+//    Name = "Sam",
+//    Age = 29,
+//    Company = new Company { Name = "Yandex" },
+//    Languages = { "english", "german" }
+//};
+
+//BsonDocument doc2 = person2.ToBsonDocument();
+//BsonDocument doc3 = person3.ToBsonDocument();
+//var users = db.GetCollection<BsonDocument>("users");
+//await users.InsertManyAsync(new List<BsonDocument> { doc2,doc3});
+
+//Получение документов из базы данных
+List<BsonDocument> userList = await users.Find(new BsonDocument()).ToListAsync();
+foreach (var user in userList)
 {
-    {"name","Tom"},
-    {"age", 38},
-    { "company", new BsonDocument{{"name" , "microsoft"}}},
-    {"languages", new BsonArray{"english", "german", "spanish" } }
-};
-Person person = BsonSerializer.Deserialize<Person>(doc);
-Console.WriteLine(person.ToJson());
+    Console.WriteLine(user);
+}
+List<Person> listUsers = await collection.Find(new BsonDocument()).ToListAsync();
+foreach (var user in listUsers)
+{
+    Console.WriteLine($"{user.Name} - {user.Age}");
+}
+long size = await collection.Find(new BsonDocument()).CountDocumentsAsync();
+Console.WriteLine(size);
+
+//Фильтрация
+//var filter = new BsonDocument { { "Name", "Tom" }, { "Years", 38 } };
+//List<BsonDocument> toms = await users.Find(filter).ToListAsync();
+//foreach(var user in toms)
+//{
+//    Console.WriteLine(user);
+//}
+
+//Логические операторы
+//Логические операторы выполняются над условиями выборки:
+
+//$or: соединяет два условия, и документ должен соответствовать одному из этих условий
+
+//$and: соединяет два условия, и документ должен соответствовать обоим условиям
+
+//$not: документ должен НЕ соответствовать условию
+
+//$nor: соединяет два условия, и документ должен НЕ соответстовать обоим условиям
+
+//$eq(равно)
+
+//$ne(не равно)
+
+//$gt(больше чем)
+
+//$lt(меньше чем)
+
+//$gte(больше или равно)
+
+//$lte(меньше или равно)
+
+//$in определяет массив значений, одно из которых должно иметь поле документа
+
+//$nin определяет массив значений, которые не должно иметь поле документа
+//var filter = new BsonDocument("$or", new BsonArray{
+
+//    new BsonDocument("Age",new BsonDocument("$gte", 33)),
+//    new BsonDocument("Name", "Tom")
+//});
+var filter = new BsonDocument(new BsonDocument("Years", new BsonDocument("$gte", 10)));
+
+var userFilter = await users.Find(filter).ToListAsync();
+foreach (var user in userFilter)
+{
+    Console.WriteLine(user);
+}
